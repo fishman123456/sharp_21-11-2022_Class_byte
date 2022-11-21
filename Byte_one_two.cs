e8a3599e0167a45c4f6c7a90e2080bc95b185f86
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-21 15:48:06 +0700

    Initial commit

---
+++ Byte_one_two.cs
@@ -0,0 +1,22 @@
+﻿
+    internal class Byte_one_two
+    {
+    public byte _byte1 { get; set; }
+    public byte _byte2 { get; set; }
+    public Byte_one_two(){} // TODO конструктор без параметров
+
+    public  Byte_one_two(byte byte1, byte byte2) // TODO конструктор с параметрами
+    {
+         this._byte1 = byte1;
+        this._byte2 = byte2;
+        
+    }
+
+
+    public override string ToString()
+    {
+        return $"\n Байт №1 : {_byte1}\t №2 : {_byte2}";
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//byte: хранит целое число от 0 до 255 и занимает 1 байт. Представлен системным типом System.Byte
+
+
+
+Byte_one_two h = new Byte_one_two(0,56);
+
+
+Console.WriteLine(h);
+
+
